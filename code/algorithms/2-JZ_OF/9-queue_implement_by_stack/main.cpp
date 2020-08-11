b7b3483978193a7a4ade50e981c56941ba1cfea3
xmcy0011 <xmcy0011@sina.com>
2020-08-11 10:49:21 +0800

    2个堆栈实现队列

--- code/algorithms/2-JZ_OF/9-queue_implement_by_stack/main.cpp
+++ code/algorithms/2-JZ_OF/9-queue_implement_by_stack/main.cpp
@@ -6,19 +6,30 @@
   */
 
 #include <stack>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
+
+class EmptyQueueException : std::exception {
+public:
+    const char *what() const _NOEXCEPT override {
+        return "empty queue";
+    }
+};
 
 template<typename T>
 class CQueue {
 public:
-    CQueue() {}
-
-    ~CQueue() {}
+    CQueue() = default;
+    ~CQueue() = default;
 
     void append_tail(const T &node);
 
-    T delete_head();
+    //T delete_head() throw(EmptyQueueException);
+    T delete_head() noexcept(false);
+    //T delete_head() throw(std::bad_exception);
 
 private:
     stack<T> stack1_;
@@ -31,7 +42,7 @@ void CQueue<T>::append_tail(const T &node) {
 }
 
 template<typename T>
-T CQueue<T>::delete_head() {
+T CQueue<T>::delete_head() noexcept(false) {
     if (stack2_.empty()) {
         while (!stack1_.empty()) {
             T &em = stack1_.top();
@@ -40,16 +51,33 @@ T CQueue<T>::delete_head() {
         }
     }
 
-    if (!stack2_.empty()) {
-        T &em = stack2_.top();
-        stack2_.pop();
-        return em;
+    if (stack2_.empty()) {
+        //throw std::runtime_error("empty queue");
+        throw EmptyQueueException();
     }
 
-    return nullptr;
+    T em = stack2_.top();
+    stack2_.pop();
+    return em;
 }
 
 
 int main() {
+    CQueue<int> queue;
+    queue.append_tail(9);
+    queue.append_tail(10);
+    queue.append_tail(11);
+
+    std::cout << "1:" << queue.delete_head() << std::endl;
+    std::cout << "2:" << queue.delete_head() << std::endl;
+    std::cout << "3:" << queue.delete_head() << std::endl;
+
+    try {
+        std::cout << "4:" << queue.delete_head() << std::endl;
+        //} catch (const std::bad_exception &e) {
+    } catch (const EmptyQueueException &e) {
+        std::cout << "Caught Exception: " << e.what() << std::endl;
+    }
+
     return 0;
 }
