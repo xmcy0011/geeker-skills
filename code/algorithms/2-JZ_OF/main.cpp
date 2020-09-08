fff13cdfe3c745e6d81fa507f0b7ae433d83f059
xmcy0011 <xmcy0011@sina.com>
2020-09-08 13:17:04 +0800

    update

--- code/algorithms/2-JZ_OF/main.cpp
+++ code/algorithms/2-JZ_OF/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cstdio>
+
+inline int add_from_1_to_n(int n) {
+    return n < 0 ? 0 : n + add_from_1_to_n(n - 1);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << add_from_1_to_n(4) << std::endl;
     return 0;
 }
