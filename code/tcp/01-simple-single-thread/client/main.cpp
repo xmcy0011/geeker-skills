01d3278179b3f31bee6ca3f639803b7291c27b29
xmcy0011 <xmcy0011@sina.com>
2021-03-03 17:40:45 +0800

    socket multi-thread examples

---
+++ code/tcp/01-simple-single-thread/client/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include <cerrno>
+#include <thread>
+#include <sys/socket.h> // bind,connect
+#include <netinet/in.h> // sockaddr_in
+#include <arpa/inet.h>  // inet_addr()
+#include <unistd.h>     // close
+
+const int kSocketError = -1;
+
+int main() {
+    int fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (fd == kSocketError) {
+        std::cout << "socket error:" << errno << std::endl;
+        return 0;
+    }
+
+    struct sockaddr_in serverIp{};
+    serverIp.sin_family = AF_INET;
+    serverIp.sin_port = htons(8088);
+    serverIp.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    std::cout << "connect remote" << std::endl;
+    int ret = ::connect(fd, (sockaddr *) &serverIp, sizeof(serverIp));
+    if (ret == kSocketError) {
+        std::cout << "connect error:" << errno << std::endl;
+        return 0;
+    }
+
+    char buffer[1024] = {0};
+    char recvBuffer[1024] = {0};
+    for (int i = 0; i < 10; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        int len = sprintf(buffer, "hello %d", i);
+        ret = ::send(fd, buffer, len, 0);
+        if (ret == kSocketError) {
+            std::cout << "send error:" << errno << std::endl;
+            break;
+        }
+
+        ret = ::recv(fd, recvBuffer, sizeof(recvBuffer), 0);
+        if (ret == kSocketError) {
+            std::cout << "send error:" << errno << std::endl;
+            break;
+        }
+        std::cout << "recv from:" << recvBuffer << std::endl;
+    }
+    ::close(fd);
+
+    return 0;
+}
