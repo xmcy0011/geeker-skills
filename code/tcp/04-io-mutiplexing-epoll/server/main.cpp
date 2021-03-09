ba361b9f971d27cbd048c2c9fbeb5fa52bd9d34a
summer sh <xmcy0011@sina.com>
2021-03-09 14:26:13 +0800

    add EPOLLET samples

--- code/tcp/04-io-mutiplexing-epoll/server/main.cpp
+++ code/tcp/04-io-mutiplexing-epoll/server/main.cpp
@@ -12,55 +12,128 @@
 
 const int kSocketError = -1;
 
-void onRead(const int &epFd, const int &fd, const int &listenFd, char recvBuffer[], int bufferLen) {
-    // 来新连接了
-    if (fd == listenFd) {
-        int clientFd = ::accept(listenFd, nullptr, nullptr);
-        if (clientFd == kSocketError) {
-            std::cout << "accept error:" << clientFd << std::endl;
-            return;
-        }
-        std::cout << "new connect coming" << std::endl;
+#define USE_ET  // 启用边缘触发
 
-        // 加入到兴趣列表
-        struct epoll_event ev{};
-        ev.events = EPOLLIN;
-        ev.data.fd = clientFd;
-        if (::epoll_ctl(epFd, EPOLL_CTL_ADD, clientFd, &ev) == kSocketError) {
-            std::cout << "epoll_ctl error:" << errno << std::endl;
-        }
+void onNewConnect(const int &epFd, const int &listenFd) {
+    int clientFd = ::accept(listenFd, nullptr, nullptr);
+    if (clientFd == kSocketError) {
+        std::cout << "accept error:" << clientFd << std::endl;
+        return;
+    }
+
+    // 加入到兴趣列表
+    struct epoll_event ev{};
+    ev.events = EPOLLIN;
+
+    // 边缘触发模式，追加标志。使用'或'(|)拼接
+#ifdef USE_ET
+    ev.events |= EPOLLET;
+    // 边缘触发模式，只支持非阻塞IO non-block
+    int ret = ::fcntl(clientFd, F_SETFL, O_NONBLOCK);
+    if (ret == kSocketError) {
+        std::cout << "fcntl error:" << errno << std::endl;
+        return;
+    }
+#endif
+
+    ev.data.fd = clientFd;
+    if (::epoll_ctl(epFd, EPOLL_CTL_ADD, clientFd, &ev) == kSocketError) {
+        std::cout << "epoll_ctl error:" << errno << std::endl;
+        ::close(clientFd);
+    }
+
+    if (ev.events & EPOLLET) {
+        std::cout << "new connect coming,set EPOLLET" << std::endl;
     } else {
-        // 获取客户端IP
-        struct sockaddr_in peer{};
-        socklen_t len = sizeof(sockaddr_in);
-        if (::getpeername(fd, (sockaddr *) &peer, &len) == kSocketError) {
-            std::cout << "getpeername error" << errno << std::endl;
-            return;
-        }
+        std::cout << "new connect coming" << std::endl;
+    }
+}
 
-        int ret = ::recv(fd, recvBuffer, bufferLen, 0);
-        if (ret == 0) { // EOF
+#ifdef USE_ET
+
+void onRead(const int &epFd, const int &fd, const int &listenFd, char recvBuffer[], int bufferLen) {
+    // 获取客户端IP
+    struct sockaddr_in peer{};
+    socklen_t len = sizeof(sockaddr_in);
+    if (::getpeername(fd, (sockaddr *) &peer, &len) == kSocketError) {
+        std::cout << "getpeername error" << errno << std::endl;
+        return;
+    }
+
+    // 边缘触发模式下，要一次性从socket的recv缓冲区中把数据读出来，
+    // 否则下一次不再触发读事件，导致数据丢失
+    // 注意：这里就会导致所谓的TCP粘包问题，应用层协议自己通过TLV处理，这里只是为了方便演示故简化
+    char *recvBufferStart = recvBuffer;
+    int ret = 0;
+    int recvTotalLen = 0;
+    while (true) {
+        ret = ::recv(fd, recvBufferStart, bufferLen - recvTotalLen, 0);
+        if (ret == 0) { // EOF，0代表对端断开连接，需要关闭socket
             std::cout << "EOF: client " << fd << " close ths connection, " << inet_ntoa(peer.sin_addr) << ":"
                       << ::ntohs(peer.sin_port) << std::endl;
             // 移除
             if (::epoll_ctl(epFd, EPOLL_CTL_DEL, fd, nullptr) == kSocketError) {
                 std::cout << "epoll_ctl error:" << errno << std::endl;
             }
-
-        } else if (ret == -1) {
-            std::cout << "recv error:" << errno << std::endl;
-            if (::epoll_ctl(epFd, EPOLL_CTL_DEL, fd, nullptr) == kSocketError) {
-                std::cout << "epoll_ctl error:" << errno << std::endl;
-            }
-
-        } else {
-            std::cout << "recv from " << inet_ntoa(peer.sin_addr) << ":" << ::ntohs(peer.sin_port)
-                      << ": " << recvBuffer << ",len=" << ret << std::endl;
-            // echo
-            ::send(fd, recvBuffer, ret, 0);
+            ::close(fd);
+            break;
+        } else if (ret < 0) {
+            break;
         }
+        recvTotalLen += ret;
+        recvBufferStart += ret;
+    }
+
+    if (ret == -1 && recvTotalLen == 0) {
+        std::cout << "recv error:" << errno << std::endl;
+        if (::epoll_ctl(epFd, EPOLL_CTL_DEL, fd, nullptr) == kSocketError) {
+            std::cout << "epoll_ctl error:" << errno << std::endl;
+        }
+        ::close(fd);
+
+    } else {
+        std::cout << "recv from " << inet_ntoa(peer.sin_addr) << ":" << ::ntohs(peer.sin_port)
+                  << ": " << recvBuffer << ",len=" << ret << std::endl;
+        // echo
+        ::send(fd, recvBuffer, ret, 0);
     }
 }
+
+#elif
+void onRead(const int &epFd, const int &fd, const int &listenFd, char recvBuffer[], int bufferLen) {
+    // 获取客户端IP
+    struct sockaddr_in peer{};
+    socklen_t len = sizeof(sockaddr_in);
+    if (::getpeername(fd, (sockaddr *) &peer, &len) == kSocketError) {
+        std::cout << "getpeername error" << errno << std::endl;
+        return;
+    }
+
+    int ret = ::recv(fd, recvBuffer, bufferLen, 0);
+    if (ret == 0) { // EOF
+        std::cout << "EOF: client " << fd << " close ths connection, " << inet_ntoa(peer.sin_addr) << ":"
+                  << ::ntohs(peer.sin_port) << std::endl;
+        // 移除
+        if (::epoll_ctl(epFd, EPOLL_CTL_DEL, fd, nullptr) == kSocketError) {
+            std::cout << "epoll_ctl error:" << errno << std::endl;
+        }
+        ::close(fd);
+
+    } else if (ret == -1) {
+        std::cout << "recv error:" << errno << std::endl;
+        if (::epoll_ctl(epFd, EPOLL_CTL_DEL, fd, nullptr) == kSocketError) {
+            std::cout << "epoll_ctl error:" << errno << std::endl;
+        }
+        ::close(fd);
+
+    } else {
+        std::cout << "recv from " << inet_ntoa(peer.sin_addr) << ":" << ::ntohs(peer.sin_port)
+                  << ": " << recvBuffer << ",len=" << ret << std::endl;
+        // echo
+        ::send(fd, recvBuffer, ret, 0);
+    }
+}
+#endif
 
 void onClose(const int &epFd, const int &fd) {
     std::cout << "remove close connection" << std::endl;
@@ -85,7 +158,11 @@ int main() {
     int epFd = 0;                       // epoll实例
     int maxFiles = 1024;                // 最大可打开文件的数量
     int fdNum = 0;                      // epoll_wait返回产生事件的描述法数量
+#ifdef USE_ET
+    char recvBuffer[10 * 1024] = {0};   // 接收缓冲区，边缘触发模式下要一次性读完，所以缓冲区要大一些
+#elif
     char recvBuffer[1024] = {0};        // 接收缓冲区
+#endif
 
     // get max open file
     maxFiles = static_cast<int>(::sysconf(_SC_OPEN_MAX));
@@ -164,8 +241,13 @@ int main() {
         for (int i = 0; i < fdNum; ++i) {
             const epoll_event &e = events[i];
             int fd = e.data.fd;
+
             if (e.events & EPOLLIN) {
-                onRead(epFd, fd, listenFd, recvBuffer, sizeof(recvBuffer));
+                if (fd == listenFd) {// 监听socket上读事件，代表新的连接到来
+                    onNewConnect(epFd, listenFd);
+                } else {
+                    onRead(epFd, fd, listenFd, recvBuffer, sizeof(recvBuffer));
+                }
             }
 #ifdef EPOLLRDHUP
             else if (e.events & EPOLLRDHUP) { // linux 2.6.17 以上
